e32d3b15e2ab19cdb70f2d89f22ec7bf73c0175b
Ranjit Kumar <ranjitkumar448@yahoo.com>
2023-07-21 16:04:53 +0530

    first commit

---
+++ apps/admin/src/app/app.module.ts
@@ -0,0 +1,112 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+import { ProductsListComponent } from './pages/products/products-list/products-list.component';
+import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
+
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { CategoriesService } from '@bluebits/products';
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { EditorModule } from 'primeng/editor';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
+import { UsersFormComponent } from './pages/users/users-form/users-form.component';
+
+const UX_MODULE = [
+  CardModule,
+  ToastModule,
+  InputTextModule,
+  TableModule,
+  ToolbarModule,
+  ButtonModule,
+  ConfirmDialogModule,
+  ColorPickerModule,
+  InputNumberModule,
+  DropdownModule,
+  InputTextareaModule,
+  InputSwitchModule,
+  EditorModule
+];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'categories',
+        component: CategoriesListComponent
+      },
+      {
+        path: 'categories/form',
+        component: CategoriesFormComponent
+      },
+      {
+        path: 'categories/form/:id',
+        component: CategoriesFormComponent
+      },
+      {
+        path: 'products',
+        component: ProductsListComponent
+      },
+      {
+        path: 'products/form',
+        component: ProductsFormComponent
+      },
+      {
+        path: 'products/form/:id',
+        component: ProductsFormComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    ShellComponent,
+    DashboardComponent,
+    CategoriesListComponent,
+    CategoriesFormComponent,
+    ProductsListComponent,
+    ProductsFormComponent,
+    UsersListComponent,
+    UsersFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+    ...UX_MODULE
+  ],
+  providers: [CategoriesService, MessageService, ConfirmationService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ apps/admin/src/app/pages/users/users-list/users-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bluebits-users-list',
+  templateUrl: './users-list.component.html',
+  styles: [
+  ]
+})
+export class UsersListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
